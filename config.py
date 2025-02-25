72bead36caa74727e9ae3140ba7974dc2f19d99f
Anime-Master-World <122656597+Anime-Master-World@users.noreply.github.com>
2025-02-25 06:12:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,13 +18,13 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("12859221", ""))
+API_HASH = environ.get("275ad23eb4cb652f402c0a08fdd81bc4", "")
+BOT_TOKEN = environ.get("7643047125:AAHYJu60iAtf0a9bTp0ZFv0XNvw-ymCxtzA", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
+BOT_USERNAME = environ.get("kkfilestorebot", "") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
@@ -35,8 +35,8 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "techvjbotz")
+DB_URI = environ.get("mongodb+srv://tgveerakumar:tgveerakumar125@clustertg.rshdi.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTG", "")
+DB_NAME = environ.get("kkfilestorebot", "techvjbotz")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
